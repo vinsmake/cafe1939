38332a703a8c0b0a9bcaca8f7796a758a49bfc66
vinsmake <ps.enrique.e@gmail.com>
2026-01-05 14:33:20 -0600

    schema added

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,14 @@
 import { z } from 'astro:content';
 
 export const BaseWPSchema = z.object({
-    id: z.number()
+    id: z.number(),
+    title: z.object({
+        rendered: z.string()
+    }),
+    content: z.object({
+        rendered: z.string()
+    }),
+    acf: z.object({
+        subtitle: z.string()
+    })
 });
