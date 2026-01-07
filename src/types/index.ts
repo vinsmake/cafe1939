57ea3d8b53789ed523aa13805440a33aaa783c14
vinsmake <ps.enrique.e@gmail.com>
2026-01-07 16:19:27 -0600

    get images supported

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,21 @@
 import { z } from 'astro:content';
 
+
+const imageSchema = z.object({
+    url: z.string(),
+    width: z.number(),
+    height: z.number()
+})
+
+const featuredImagesSchema = z.object({
+    thumbnail: imageSchema,
+    medium: imageSchema,
+    medium_large: imageSchema,
+    large: imageSchema,
+    full: imageSchema,
+});
+
+
 export const BaseWPSchema = z.object({
     id: z.number(),
     title: z.object({
@@ -8,6 +24,7 @@ export const BaseWPSchema = z.object({
     content: z.object({
         rendered: z.string()
     }),
+    featured_images: featuredImagesSchema,
     acf: z.object({
         subtitle: z.string()
     })
